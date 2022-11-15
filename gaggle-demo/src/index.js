f64858965207d9a11fa166b07a85de4e31cf17c3
Chuck Leone <chuckleone@gmail.com>
2022-11-15 13:33:15 -0500

    cleanup
    cleanup imports

--- gaggle-demo/src/index.js
+++ gaggle-demo/src/index.js
@@ -1,29 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-import Root from "./routes/root";
-import Error from "./routes/error";
-import IncidentsPage from "./pages/IncidentsPage";
-import ForgotPasswordPage from "./pages/ForgotPasswordPage";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from './routes/root';
+import Error from './routes/error';
+import IncidentsPage from './pages/IncidentsPage';
+import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     errorElement: <Error />,
   },
   {
-    path: "incidents/",
+    path: 'incidents/',
     element: <IncidentsPage />,
   },
   {
-    path: "forgot-password/",
+    path: 'forgot-password/',
     element: <ForgotPasswordPage />,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
--- gaggle-demo/src/routes/root.jsx
+++ gaggle-demo/src/routes/root.jsx
@@ -1,5 +1,4 @@
-import { Outlet, Link } from "react-router-dom";
-import SignOnPage from "../pages/SignOnPage";
+import SignOnPage from '../pages/SignOnPage';
 
 export default function Root() {
   return (
